0bf47cf0d4ffc7596d5373b5eff1b529d4050279
Visud <visudchang@berkeley.edu>
2025-04-12 17:56:11 -0700

    Visualized propagation

--- sim/main.py
+++ sim/main.py
@@ -5,12 +5,14 @@ from poliastro.plotting.static import StaticOrbitPlotter
 import matplotlib.pyplot as plt
 from poliastro.plotting import OrbitPlotter3D
 from plotly.offline import plot
+import plotly.graph_objects as go
 import sys
 import os
+import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from sim.config import mission_config as cfg
 
-orb0 = Orbit.from_classical(
+orbit = Orbit.from_classical(
     cfg["attractor"],
     cfg["a"],
     cfg["ecc"],
@@ -23,9 +25,28 @@ orb0 = Orbit.from_classical(
 
 time_step = 60 * u.s
 duration = 30 * 60 * u.s
+times = [time_step * i for i in range(int(duration / time_step))]
 
+def live_telemetry():
+    for t in times:
+        propogated_orbit = orbit.propagate(t)
+        r = propogated_orbit.r
+        v = propogated_orbit.v
+        print(f"[T + {t.to(u.min)}] Position: {r}, Velocity: {v}")
+        time.sleep(1)
 
-fig, ax = plt.subplots(figsize=(6, 6))
-plotter = StaticOrbitPlotter(ax)
-plotter.plot(orb0, label="LEO Orbit")
-plt.show()
+coords = [orbit.propagate(t).r.to(u.km).value for t in times]
+xs, ys, zs = zip(*coords)
+
+fig = go.Figure(data = go.Scatter3d(
+    x = xs, y = ys, z = zs,
+    mode = 'lines+markers',
+    marker = dict(size = 4),
+    line = dict(width = 2)
+))
+
+fig.update_layout(
+    title = "Orbit Propagation",
+)
+
+fig.show()
