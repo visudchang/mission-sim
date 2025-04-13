93b0f3c6eb0ab852694b6e848cec9786d347ffc7
Visud <visudchang@berkeley.edu>
2025-04-12 19:37:02 -0700

    Done with Day 1

--- sim/animated_orbit.py
+++ sim/animated_orbit.py
@@ -12,65 +12,65 @@ orbit = Orbit.from_classical(Earth,
                              0 * u.deg,
                              0 * u.deg,
                              0 * u.deg,
-                             epoch=epoch)
+                             epoch = epoch)
 
 # Propagate orbit and collect positions
-times = [i * 60 * u.s for i in range(61)]  # 60 minutes
+times = [i * 60 * u.s for i in range(181)]  # 180 minutes
 coords = [orbit.propagate(t).r.to(u.km).value for t in times]
 xs, ys, zs = zip(*coords)
 
 # Create frames (each with Earth + satellite position + trail)
 frames = [
     go.Frame(
-        data=[
+        data = [
             earth_surface,
             go.Scatter3d(
-                x=[xs[i]], y=[ys[i]], z=[zs[i]],
-                mode='markers',
-                marker=dict(size=6, color='red'),
-                name='Satellite'
+                x = [xs[i]], y = [ys[i]], z = [zs[i]],
+                mode = 'markers',
+                marker = dict(size=6, color='red'),
+                name = 'Satellite'
             ),
             go.Scatter3d(
-                x=xs[:i+1], y=ys[:i+1], z=zs[:i+1],
-                mode='lines',
-                line=dict(width=2, color='white'),
-                name='Orbit Path'
+                x = xs[:i+1], y = ys[:i+1], z = zs[:i+1],
+                mode = 'lines',
+                line = dict(width = 2, color = 'white'),
+                name = 'Orbit Path'
             )
         ],
-        name=f'frame{i}'
+        name = f'frame{i}'
     )
     for i in range(len(xs))
 ]
 
 # Base figure
 fig = go.Figure(
-    data=[
+    data = [
         earth_surface,
-        go.Scatter3d(x=[xs[0]], y=[ys[0]], z=[zs[0]],
-                     mode='markers', marker=dict(size=6, color='red')),
-        go.Scatter3d(x=[], y=[], z=[], mode='lines',
-                     line=dict(width=2, color='white'))
+        go.Scatter3d(x = [xs[0]], y = [ys[0]], z = [zs[0]],
+                     mode = 'markers', marker = dict(size = 6, color = 'red')),
+        go.Scatter3d(x = [], y = [], z = [], mode = 'lines',
+                     line = dict(width = 2, color = 'white'))
     ],
     layout=go.Layout(
-        title="Animated CubeSat Orbit Around Earth",
-        scene=dict(
-            xaxis_title='X (km)',
-            yaxis_title='Y (km)',
-            zaxis_title='Z (km)',
-            aspectmode='data'
+        title = "Animated Orbit Around Earth",
+        scene = dict(
+            xaxis_title = 'X (km)',
+            yaxis_title = 'Y (km)',
+            zaxis_title = 'Z (km)',
+            aspectmode = 'data'
         ),
-        updatemenus=[dict(
-            type='buttons',
-            buttons=[dict(label='Play',
-                          method='animate',
-                          args=[None, {
-                              'frame': {'duration': 100, 'redraw': True},
+        updatemenus = [dict(
+            type = 'buttons',
+            buttons = [dict(label = 'Play',
+                          method = 'animate',
+                          args = [None, {
+                              'frame': {'duration': 75, 'redraw': True},
                               'fromcurrent': True,
                               'mode': 'immediate'}])],
-            showactive=False
+            showactive = False
         )]
     ),
-    frames=frames
+    frames = frames
 )
 
 fig.show()
