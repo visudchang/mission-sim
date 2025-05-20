617a16e87a50039f64d7f865242e2570f50cad42
Visud <visudchang@berkeley.edu>
2025-05-19 19:47:48 -0700

    Created go file

--- go/src/hello-telemetry.go
+++ go/src/hello-telemetry.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello telemetry!")
+	conn, err := net.Dial("tcp", "127.0.0.1:65432")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	message := "GET_STATUS"
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		fmt.Println("Error sending:", err)
+		os.Exit(1)
+	}
+
+	buffer := make([]byte, 1024)
+	n, _ := conn.Read(buffer)
+	fmt.Println("Received:", string(buffer[:n]))
 }
