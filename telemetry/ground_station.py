0fe0181f454c239e2ad11f4fa2c282d8d1fe1632
Visud <visudchang@berkeley.edu>
2025-08-08 01:53:15 -0700

    Update app to listen on port 8080 for Fly.io

--- telemetry/ground_station.py
+++ telemetry/ground_station.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
         )
         socket_thread.start()
 
-        app.run(host="0.0.0.0", port=5000, debug=False)
+        app.run(host="0.0.0.0", port=8080, debug=False)
 
 
 
